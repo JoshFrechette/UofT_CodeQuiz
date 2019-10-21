d2c10480051f22e76087bec711e8903ce28c347a
Josh Frechette <josh.f.design@gmail.com>
2019-10-20 23:54:53 -0400

    More progress alon g the new form for the assignemnt.

--- assets/questions.js
+++ assets/questions.js
@@ -5,16 +5,7 @@ var timer = 60;
 var QuizGAme = function() {
 // Game variables here
 
-// Timer countdown script
-var timeleft = 60;
-  countDown = setInterval(function(){
-  document.getElementById("timer").innerHTML = "Time " + timeleft + " seconds";
-  timeleft -= 1;
-  if(timeleft <= 0){
-    clearInterval(countDown);
-    document.getElementById("timer").innerHTML = "Time Out!"
-  }
-}, 1000);
+
 
 // Hint provided from the assignment file
 var questions = [
@@ -36,11 +27,37 @@ var questions = [
 ];
 
   function init() {
-      //"init-page content" First content to come up
-      var btn = document.createElement("BUTTON");
-      start()
+  //"init-page content" First content to come up
+    
+    var head = document.getElementById("h1");
+    var headLevel = document.createTextNode("Coding Quiz Challenge");
+
+    var paragraph = document.getElementById("p");
+    var text = document.createTextNode("Try to answer the following code related questions within the time limit. Keep in mind that incorrect answers will penalize your scoretime by ten seconds!");
+
+    paragraph.appendChild(text);
+    head.appendChild(headLevel);
+
+    var startBtn = document.createElement("BUTTON"); // Create a button for the user to start the quiz || starts the timer
+    startBtn.innerHTML = "Start Quiz";
+    document.body.appendChild(startBtn);
+
+    startBtn.onclick = start()
   }
   function start() {
+    //Start the timer
+    // Timer countdown script
+    var timeleft = 60;
+    countDown = setInterval(function(){
+    document.getElementById("timer").innerHTML = "Time " + timeleft + " seconds";
+    timeleft -= 1;
+    if(timeleft <= 0){
+    clearInterval(countDown);
+    document.getElementById("timer").innerHTML = "Time Out!"
+  }
+  }, 1000);
+
+
       changeScore()
   }
   function changeScore() {
