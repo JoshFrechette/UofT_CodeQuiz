f65e80155d9e21cd8afe392873e890f0b78896f3
Josh Frechette <josh.f.design@gmail.com>
2019-11-08 18:59:47 -0500

    Looking over old code, looking for looping answer there

--- assets/questions.js
+++ assets/questions.js
@@ -1,10 +1,28 @@
 //Global variables
 //var timeLeft = 0;
-var timer = 60;
-var head = 0;;
+var timeLeft = 60;
+
 var score = 0;
 var questionIndex = 0;
 var highScores = [];
+
+var questions = [
+  {
+  title: "Commonly used data types DO NOT include:",
+  choices: ["strings", "booleans", "alerts", "numbers"],
+  answer: "alerts"
+  },
+  {
+  title: "The condition in an if / else statement is enclosed within ____.",
+  choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+  answer: "parentheses"
+  },
+  {
+  title: "Which typing convention is widely accepted as the norm in the coding community?",
+  choices: ["giraffe bumps", "camel case", "lemur tail", "squirrel sprint"],
+  answer: "camel case"
+  },
+];
 
 
 //A way presented in class
@@ -38,60 +56,44 @@ var QuizGAme = function() {
   console.log("start screen has loaded")
          //returns content to empty state
 
-                  var countDown = document.getElementById("timer").innerHTML = "Time " + timeLeft + " seconds";
-                  timeLeft -= 1;
+  var countDown = document.getElementById("timer").innerHTML = "Time " + timeLeft + " seconds";
+  timeLeft -= 1;
 
-                  var timeLeft = 60;
-                  //Timer countdown script, to start counting down after 
-                  countDown = setInterval(function(){
-                  document.getElementById("timer").innerHTML = "Time " + timeLeft + " seconds";
-                  timeLeft -= 1;
-                    
-                    if(timeLeft <= 0){
-                    clearInterval(countDown);
-                    document.getElementById("timer").innerHTML = "Time Out!";
-                    }
-                    }, 1000);
+  var timeLeft = 60;
+  //Timer countdown script, to start counting down after 
+  countDown = setInterval(function(){
+  document.getElementById("timer").innerHTML = "Time " + timeLeft + " seconds";
+  timeLeft -= 1;
+    
+    if(timeLeft <= 0){
+    clearInterval(countDown);
+    document.getElementById("timer").innerHTML = "Time Out!";
+    }
+    }, 1000);
 
-                    var questions = [
-                      {
-                      title: "Commonly used data types DO NOT include:",
-                      choices: ["strings", "booleans", "alerts", "numbers"],
-                      answer: "alerts"
-                      },
-                      {
-                      title: "The condition in an if / else statement is enclosed within ____.",
-                      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-                      answer: "parentheses"
-                      },
-                      {
-                      title: "Which typing convention is widely accepted as the norm in the coding community?",
-                      choices: ["giraffe bumps", "camel case", "lemur tail", "squirrel sprint"],
-                      answer: "camel case"
-                      },
-                    ];
+ 
 
-          console.log(questions)
+  console.log(questions)
 
-                  if (questionIndex <= (questions.length)) { //Keep asking questions until there are no more
-                    document.querySelector("#title").innerHTML = questions[questionIndex].title;
-                    //Clear
-                    //var possibleAnswer = questions[choices];
-                    for (i = 0; i <= questions.length; i++) {
-                     
-                        $(".possible-answers").append($("<button>").text([i+ 1] + " " + questions[questionIndex].choices[i]).on("click"));
-                        userAnswer = $(this).text(questions[questionIndex].choices[i]);
-                        console.log(userAnswer)
-                    }
-                    /*if (userAnswer != answer)
-                      timeLeft = timeLeft + 15;*/
-                  }   
-                  if (questionIndex === questions.length || timer === 0) {// Determines if the game has ended, either by answering all the questions or if time has run out. End of this block of code ==> on to next
-                  changeScore()
-                  }
-
-            head = document.getElementById("h2");
+          if (questionIndex <= (questions.length)) { //Keep asking questions until there are no more
+            document.querySelector("#title").innerHTML = questions[questionIndex].title;
+            //Clear
+            //var possibleAnswer = questions[choices];
+            for (i = 0; i <= questions.length; i++) {
+              
+                $(".possible-answers").append($("<button>").text(questions[questionIndex].choices[i]).on("click"));
+                userAnswer = $(this).text(questions[questionIndex].choices[i]);
+                console.log(userAnswer)
+            }
+            /*if (userAnswer != answer)
+              timeLeft = timeLeft + 15;*/
+          }   
+          if (questionIndex === questions.length || timer === 0) {// Determines if the game has ended, either by answering all the questions or if time has run out. End of this block of code ==> on to next
+          changeScore()
           }
+
+    head = document.getElementById("h2");
+  }
         
           //questions() // Where did this come from?
       // Hint provided from the assignment file
