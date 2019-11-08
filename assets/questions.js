0f134cbe7bd41f5f7cd0d5608eb9e4c6e2187338
Josh Frechette <josh.f.design@gmail.com>
2019-11-08 18:44:25 -0500

    TA Sean hard-pressed value of i into code, still having trouble looping the questions through

--- assets/questions.js
+++ assets/questions.js
@@ -1,118 +1,165 @@
 //Global variables
-//var timeLeft = 0;
-var timer = 60;
-var head = 0;;
-var score = 0;
+
 var questionIndex = 0;
+var score = "";
 var highScores = [];
+var timeLeft = 60;
+
+var questions = [
+  {
+  title: "Commonly used data types DO NOT include:",
+  choices: ["strings", "booleans", "alerts", "numbers"],
+  answer: "alerts"
+  },
+  {
+  title: "The condition in an if / else statement is enclosed within ____.",
+  choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+  answer: "parentheses"
+  },
+  {
+  title: "Which typing convention is widely accepted as the norm in the coding community?",
+  choices: ["giraffe bumps", "camel case", "lemur tail", "squirrel sprint"],
+  answer: "camel case"
+  },
+];
 
 
-//A way presented in class
+
 var QuizGAme = function() {
 // Game variables here
-//init()
-//==================================================================
-  //function init() {
-  console.log("ready");
-//"init-page content" First content to come up
-        head = document.getElementById("h1");
-        var headLevel = document.createTextNode("Coding Quiz Challenge");
-        head.appendChild(headLevel);
+console.log("Game has started")
 
-        var paragraph = document.getElementById("p");
-        var text = document.createTextNode("Try to answer the following code related questions within the time limit. Keep in mind that incorrect answers will penalize your scoretime by ten seconds!");
-        paragraph.appendChild(text);
-        
-        $(".init-page").append($("<button>").text("Start Quiz").addClass("button").on("click", start))
-        /*var startBtn = document.createElement("BUTTON"); // Create a button for the user to start the quiz || starts the timer
-        startBtn.className = "button"; // Give  a class to the anme so that it can be styled
-        startBtn.innerHTML = "Start Quiz"; //Text for the button function
-        document.body.appendChild(startBtn); //Place the button in the page
+//===================Landing Page Section=============================
+head = document.getElementById("h1");
+var headLevel = document.createTextNode("Coding Quiz Challenge");
+head.appendChild(headLevel);
 
-        document.addEventListener("click", start);*/
-      
-  //}
-//==================================================================
-  function start() {
-    $(".init-page").empty();
-  console.log("start screen has loaded")
-         //returns content to empty state
+var paragraph = document.getElementById("p");
+var text = document.createTextNode("Try to answer the following code related questions within the time limit. Keep in mind that incorrect answers will penalize your scoretime by ten seconds!");
+paragraph.appendChild(text);
 
-                  var countDown = document.getElementById("timer").innerHTML = "Time " + timeLeft + " seconds";
-                  timeLeft -= 1;
+$(".init-page").append($("<button>").text("Start Quiz").addClass("button").on("click", start))
+  
+//===================Quiz Game Section================================
 
-                  var timeLeft = 60;
-                  //Timer countdown script, to start counting down after 
-                  countDown = setInterval(function(){
-                  document.getElementById("timer").innerHTML = "Time " + timeLeft + " seconds";
-                  timeLeft -= 1;
-                    
-                    if(timeLeft <= 0){
-                    clearInterval(countDown);
-                    document.getElementById("timer").innerHTML = "Time Out!";
-                    }
-                    }, 1000);
+function start() {
+$(".init-page").empty();//returns content to empty state
+console.log("start screen has loaded")
+  askQuestions();
+  timeLeft = 60;
+  //Timer countdown script, to start counting down after 
+  countDown = setInterval(function(){
 
-                    var questions = [
-                      {
-                      title: "Commonly used data types DO NOT include:",
-                      choices: ["strings", "booleans", "alerts", "numbers"],
-                      answer: "alerts"
-                      },
-                      {
-                      title: "The condition in an if / else statement is enclosed within ____.",
-                      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-                      answer: "parentheses"
-                      },
-                      {
-                      title: "Which typing convention is widely accepted as the norm in the coding community?",
-                      choices: ["giraffe bumps", "camel case", "lemur tail", "squirrel sprint"],
-                      answer: "camel case"
-                      },
-                    ];
+  document.getElementById("timer").innerHTML = "Time " + timeLeft + " seconds";
+  timeLeft -= 1;
 
-          console.log(questions)
-
-                  if (questionIndex <= (questions.length)) { //Keep asking questions until there are no more
-                    document.querySelector("#title").innerHTML = questions[questionIndex].title;
-                    //Clear
-                    //var possibleAnswer = questions[choices];
-                    for (i = 0; i <= questions.length; i++) {
-                     
-                        $(".possible-answers").append($("<button>").text([i+ 1] + " " + questions[questionIndex].choices[i]).on("click"));
-                        userAnswer = $(this).text(questions[questionIndex].choices[i]);
-                        console.log(userAnswer)
-                    }
-                    /*if (userAnswer != answer)
-                      timeLeft = timeLeft + 15;*/
-                  }   
-                  if (questionIndex === questions.length || timer === 0) {// Determines if the game has ended, either by answering all the questions or if time has run out. End of this block of code ==> on to next
-                  changeScore()
-                  }
-
-            head = document.getElementById("h2");
-          }
-        
-          //questions() // Where did this come from?
-      // Hint provided from the assignment file
-
-
-
-  //===================================================================
-  function changeScore() {
-      console.log("changeScore screen has loaded")
-      // Log initals 
-      //endGame()
-  }
-      //init();// Return to Homescreen when button is pushed
+    if (timeLeft <= 0){
+    clearInterval(countDown);
+    document.getElementById("timer").innerHTML = "Time Out!";
+    }
+    if (timeLeft == 0){
+      changeScore() //Time runs out, go to changeScore page
+    }
+    }, 1000);
 }
 
-  //==================================================================
-// *Fix code*
-// When elements loaded, start init()
-//$(document.ready(QuizGAme()))
+
+    // Ask the questions
+
+function askQuestions(){
+for (i = 0; i <= questions[questionIndex].choices.length -1; i++) {  
+  
+    document.querySelector("#title").innerHTML = questions[questionIndex].title;//presents the question to the user   
+    console.log(questions[questionIndex].choices[i],questions[questionIndex].answer) 
+  $(".possible-answers").append($(`<button id=${i}>`).text(questions[questionIndex].choices[i])
+    .on("click", function (event){//Sean helped by hard pressing the "i" value into the code.
+console.log(questions)
+console.log(event.target.id)
+      let currentQuestion = questions[questionIndex].choices[event.target.id]
+      let currentAnswer = questions[questionIndex].answer
+      console.log(currentQuestion, currentAnswer)
+     
+    if (currentAnswer == currentQuestion){
+      $("#prompt").append("<hr>").append($("<p>").text("Right!!!"))
+    }  
+    if (currentAnswer != currentQuestion){
+      timeLeft = timeLeft - 10;
+      $("#prompt").append("<hr>").append($("<p>").text("Wrong!!!")) 
+    }
+    //$(".start-page").empty();//returns content to empty state
+    
+    }))
+} 
+
+//questions[questionIndex] = questions[questionIndex+1];
+//console.log(questions[questionIndex])
+}                        
+    }
+
+/*function getAnswer(userAnswer,answer) {
+console.log(userAnswer,answer);
+console.log("top of get answer")
+    if (userAnswer != answer){
+      console.log(userAnswer)
+      console.log(answer)
+      timeLeft = timeLeft - 10;
+      $("#prompt").append("<hr>").append($("<p>").text("Wrong!!!"))  
+                              
+    }   
+    
+    if (userAnswer){
+      $("#prompt").append("<hr>").append($("<p>").text("Right!!!"))
+
+    }   
+    //$(".start-page").empty();
+    //askQuestions(questions[questionIndex+1])
+    //questions[questionIndex] = questions[questionIndex+1];
+}*/
+
+
+
+
+//=================Change Score Section===============================
+
+/*function changeScore() {
+$(".start-page").empty();
+    $(".changeScore-page").append("<h2>").text("Enter your Initials")
+      .append("<form>").addClass("initials")
+        .append("<input>")
+        .append("<button>").attr("submit").on("click", addScore())
+    console.log("changeScore screen has loaded")
+    // Log initals 
+
+    function addScore() {
+
+    
+    score = document.getElementById("timer").innerHTML;// Copies the time left to become the score
+    //let name  = // Get initials, add score, push to highScore array as a string
+    }
+
+
+}
+
+   /* if (questionIndex <= questions[questionIndex].choices.length) {//Keep asking questions until there are no more
+      console.log(questionIndex, questions[questionIndex].choices.length)
+    askQuestions(questions[i + 1]) 
+    } 
+    else {
+    changeScore()
+    }*/
+
+//askQuestions(questions +1)
+//===================================================================
 QuizGAme()
 console.log("document has been loaded");
+/*
+//============Leader Board Section======================================
+function leaderBoard(){
 
+  for (i = 0; i < highScores.length; i++) {
+    $("#highscores").append("<p>").text(highScores[i])
+  }
+}
+*/
 
  
